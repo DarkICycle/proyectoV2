57bc158d4b6ad33228977c9dad792d3ef3a2998d
Samuel Gutierrez <samuelgutierrez.figueredo@gmail.com>
2025-10-10 15:02:13 -0500

    Dry Code - Recursividad

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -22,16 +23,10 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def article_params
-    params.require(:article).permit(:title, :description)
-  end
-
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article =Article.find(params[:id])
     if @article.update(article_params)
       flash[:noticed] = "Article was update successfully."
       redirect_to @article
@@ -41,9 +36,17 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article =Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
 
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
