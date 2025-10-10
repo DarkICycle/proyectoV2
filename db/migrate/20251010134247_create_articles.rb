88a10fad072a4510bd3341609c5c573e93d34a6b
Samuel Gutierrez <samuelgutierrez.figueredo@gmail.com>
2025-10-10 09:09:39 -0500

    Correccion atributo title

--- db/migrate/20251010134247_create_articles.rb
+++ db/migrate/20251010134247_create_articles.rb
@@ -1,7 +1,7 @@
 class CreateArticles < ActiveRecord::Migration[8.0]
   def change
     create_table :articles do |t|
-      t.string :tittle
+      t.string :title
       t.text :description
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_10_134247) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_10_134910) do
   create_table "articles", force: :cascade do |t|
-    t.string "tittle"
+    t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
