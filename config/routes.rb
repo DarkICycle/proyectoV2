5402e962bfcf5ee1ac7114ac9bab5412ef461200
Samuel Gutierrez <samuelgutierrez.figueredo@gmail.com>
2025-10-10 14:19:54 -0500

    Link eliminacion de articulo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:index, :show, :new, :create, :update, :edit]
+  resources :articles, only: [:index, :show, :new, :create, :update, :edit, :destroy]
   root 'pages#home'
   get 'about', to: 'pages#about'
 end
