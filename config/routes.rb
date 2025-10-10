edf02c0c424e22e6f4886d470addf428235a1e24
Samuel Gutierrez <samuelgutierrez.figueredo@gmail.com>
2025-10-10 13:44:42 -0500

    Edicion de articles funcional

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:index, :show, :new, :create, :update]
+  resources :articles, only: [:index, :show, :new, :create, :update, :edit]
   root 'pages#home'
   get 'about', to: 'pages#about'
 end
