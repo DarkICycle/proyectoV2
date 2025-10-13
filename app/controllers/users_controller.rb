6bc6f458b1245024169f501aa75db62d8feee4b9
Samuel Gutierrez <samuelgutierrez.figueredo@gmail.com>
2025-10-13 17:11:25 -0500

    Formulario funcional de users nuevos y redireccionamiento de home a new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,18 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Bienvenido a Alpha Blog #{@user.username}!, te has registrado correctamente"
+      redirect_to articles_path
+    else
+      render 'new', status: :unprocessable_entity 
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
