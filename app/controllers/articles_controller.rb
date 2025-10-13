da7c58e7c852bc591af0686045d44fbf4583982b
Samuel Gutierrez <samuelgutierrez.figueredo@gmail.com>
2025-10-13 15:56:48 -0500

    Añadido uno-a-muchos asociacion

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-
+    @article.user = User.first
     if @article.save
       flash[:notice] = "Artículo creado correctamente"
       redirect_to @article
