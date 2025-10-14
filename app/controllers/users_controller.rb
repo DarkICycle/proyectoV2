5bc96ef6d35ae5d88f1c63d7f85d2a22cc910c75
Samuel Gutierrez <samuelgutierrez.figueredo@gmail.com>
2025-10-14 09:40:50 -0500

    Permanencia de session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [ :show, :edit, :update ]
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -14,31 +14,34 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Datos atualizados!"
       redirect_to @user
     else
-      render 'new', status: :unprocessable_entity 
+      render "new", status: :unprocessable_entity
     end
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Bienvenido a Alpha Blog #{@user.username}!, te has registrado correctamente"
       redirect_to articles_path
     else
-      render 'new', status: :unprocessable_entity 
+      render "new", status: :unprocessable_entity
     end
   end
 
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
