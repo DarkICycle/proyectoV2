74e75ceb5d8623d679c38509c9714f23ca64fdda
Samuel Gutierrez <samuelgutierrez.figueredo@gmail.com>
2025-10-14 15:43:38 -0500

    creacion de categorias, tabla, modelo

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_14_161139) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_14_192721) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
