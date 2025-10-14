9429dc59fe4b673b19f3848f2827cc67c95b5925
Samuel Gutierrez <samuelgutierrez.figueredo@gmail.com>
2025-10-14 16:04:23 -0500

    Creacion de categoria

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,30 @@
 class CategoriesController < ApplicationController
 
   def new
-    
+    @category = Category.new
   end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "La categoria fue creada correctamente"
+      redirect_to @category
+    else
+      render 'new', status: :unprocessable_entity 
+    end
+  end
+
   def index
     
   end
+
   def show
     
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
