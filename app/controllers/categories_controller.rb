dab31905c069b4843954c1e988f44ee4a278720f
Samuel Gutierrez <samuelgutierrez.figueredo@gmail.com>
2025-10-15 09:57:05 -0500

    Admin y requerimientos -incompleto-

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [ :index, :show ]
 
   def new
     @category = Category.new
@@ -10,12 +11,13 @@ class CategoriesController < ApplicationController
       flash[:notice] = "La categoria fue creada correctamente"
       redirect_to @category
     else
-      render 'new', status: :unprocessable_entity 
+      render "new", status: :unprocessable_entity
     end
   end
 
   def index
-    
+    @categories = Category.all
+    @categories = Category.paginate(page: params[:page], per_page: 5)
   end
 
   def show
@@ -26,5 +28,12 @@ class CategoriesController < ApplicationController
 
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    if !(logged_in && current_user.admin)
+      flash[:alert] = "Solo administradores pueden ejecutar esta acción"
+      redirect_to categories_path
+    end
   end
 end
